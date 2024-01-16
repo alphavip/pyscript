e990f2ee0da7869198d795b3faaedafa0615465d
alphachen <sdlc86@gmail.com>
2024-01-16 16:39:57 +0800

    fix default download dir

--- myutil.py
+++ myutil.py
@@ -35,3 +35,12 @@ def return_last_month_fl_day():
     last_month_last_day = first_day - datetime.timedelta(days=1)
     last_month_first_day = last_month_last_day.replace(day=1)
     return last_month_first_day.strftime("%y%m"), last_month_first_day.strftime("%y%m%d"), last_month_last_day.strftime("%y%m%d")
+
+
+def myhomedir():
+    return os.path.expanduser("~")
+
+
+# print(os.environ["HOME"])
+# print(os.path.expandvars("$HOME"))
+# print(os.path.expanduser("~"))
